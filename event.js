3a3c0f0c857533bb5d108f050d562f5847e17faf
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-02 16:02:09 +0530

    event.js

---
+++ event.js
@@ -0,0 +1,4 @@
+const ul = document.querySelector('.items');
+ul.firstElementChild.textContent = 'HELLO';
+ul.firstElementChild.style.background='green';
+ul.children[1].style.background = 'yellow';
