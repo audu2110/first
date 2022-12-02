25c3894cfa644873928088a22a9c200a664e932b
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-02 16:03:22 +0530

    Update event.js

--- event.js
+++ event.js
@@ -2,3 +2,7 @@ const ul = document.querySelector('.items');
 ul.firstElementChild.textContent = 'HELLO';
 ul.firstElementChild.style.background='green';
 ul.children[1].style.background = 'yellow';
+
+function onClick(){
+    alert();
+}
